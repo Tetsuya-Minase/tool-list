f3b4caf81e94f57c90950e2ce433bf447b30d347
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2022-01-08 16:08:37 +0900

    feat: remove css & add setting for using linaria

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withLinaria = require('next-linaria');
+module.exports = withLinaria({
   reactStrictMode: true,
-}
+  linaria: {
+    // linariaのオプションはここに追加
+  },
+  // webpackのオプションはここに追加
+  webpack(config, options) {
+    return config;
+  }
+});
