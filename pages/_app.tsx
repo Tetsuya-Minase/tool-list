587d6940824c085e54e4a3d0fc66c3d68ad2570e
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2022-01-09 16:39:36 +0900

    style: fix page layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import 'modern-normalize/modern-normalize.css';
 import '../styles/reset.css';
 import type {AppProps} from 'next/app'
+import { styled } from 'linaria/react';
 import {Header} from '../components/header/header';
+
+const ContentsWrapper = styled.div`
+  width: 1080px;
+  margin: 0 auto;
+`;
 
 function MyApp({Component, pageProps}: AppProps) {
   return <>
     <Header/>
-    <Component {...pageProps} />
+    <ContentsWrapper>
+      <Component {...pageProps} />
+    </ContentsWrapper>
   </>
 }
 
