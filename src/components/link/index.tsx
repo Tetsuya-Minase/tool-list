6c22c4904ceeb2a7eee2c3c7e48e90f2ce215d94
Tetsuya-Minase <tetsuya.minasen@gmail.com>
2022-04-29 19:01:05 +0900

    refactor: directory structure and add LinkComponent.

---
+++ src/components/link/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NextLink from 'next/link';
+
+type Props = React.ComponentProps<'a'> & {readonly text: string};
+
+export const Link: React.FC<Props> = ({href, text}) => {
+  if (!href) {
+    return <span>{text}</span>;
+  }
+  return <NextLink href={href}>
+    <a>{text}</a>
+  </NextLink>;
+};
